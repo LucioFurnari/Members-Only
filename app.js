3f0b05967d30d6e97dfec114270163de9332177a
LucioFurnari <luciofurnari99@gmail.com>
2023-11-20 15:10:19 -0300

    Import router, remove express-validator

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const { body, validationResult } = require('express-validator');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+
+// Import router
+const Router = require('./routes/index');
 
 mongoose.connect(process.env.MONGODB_URL);
 const db = mongoose.connection;
@@ -20,5 +22,7 @@ app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/', Router);
 
 app.listen(3000, () => console.log('app listening on port 3000!'));
