7716c232e1546c3f77c56b7bc5482ec9206ed595
LucioFurnari <luciofurnari99@gmail.com>
2023-11-20 14:39:39 -0300

    Create User model

---
+++ models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = Schema({
+  name: { type: String, required: true, minLength: 3, maxLength: 60 },
+  lastName: { type: String, required: true, minLength: 3, maxLength: 60 },
+  email: { type: String, required: true, minLength: 3, maxLength: 60 },
+  password: { type: String, required: true },
+  membership: { type: Boolean, required: true },
+  isAdmin: { type: Boolean, required: true },
+});
+
+module.exports = mongoose.model('User', UserSchema);
