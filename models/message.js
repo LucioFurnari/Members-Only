3b0a4633f6acba242fa9a3ef0339a09fb0158250
LucioFurnari <luciofurnari99@gmail.com>
2023-11-20 14:20:45 -0300

    Create schema and model for Messages

---
+++ models/message.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const MessageSchema = Schema({
+  title: { type: String, required: true, minLength: 1, maxLength: 100 },
+  text: { type: String, required: true, minLength: 1, maxLength: 100 },
+  timestamp: { type: Date, default: Date.now, required: true },
+  user: { typ: Schema.type.ObjectId, required: true, ref: 'User' },
+});
+
+// Export model //
+module.exports = mongoose.model('Message', MessageSchema);
