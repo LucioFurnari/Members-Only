b511a32f1905a5d5c3e5c5bc2f5703ffe8b755a2
LucioFurnari <luciofurnari99@gmail.com>
2023-11-20 19:48:31 -0300

    Change min length of name and last name

--- models/user.js
+++ models/user.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const UserSchema = Schema({
-  name: { type: String, required: true, minLength: 3, maxLength: 60 },
-  lastName: { type: String, required: true, minLength: 3, maxLength: 60 },
+  name: { type: String, required: true, minLength: 1, maxLength: 60 },
+  lastName: { type: String, required: true, minLength: 1, maxLength: 60 },
   email: { type: String, required: true, minLength: 3, maxLength: 60 },
   password: { type: String, required: true },
   isMember: { type: Boolean, required: true },
