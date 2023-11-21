2be020a78db6dbcee4dfc6baf0186bb562f8e2de
LucioFurnari <luciofurnari99@gmail.com>
2023-11-21 15:11:57 -0300

    In error, maintain inputs values

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -12,13 +12,22 @@ exports.user_create_post = [
   body('lastName', 'The last name should not be empty').trim().isLength({ min: 1}).escape(),
   body('email', 'The email should not be empty and has to be an email').trim().isLength({ min: 1}).isEmail().escape(),
   body('password', 'The password should not be empty').trim().isLength({ min: 1}).escape(),
-  
+  body('confirm_password', 'The password is not the same').custom((value, { req }) => {
+    return value === req.body.password;
+  }),
   asyncHandler(async (req, res, next) => {
     try {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
-        res.render('sign-up', { errors: errors.array() });
+        res.render('sign-up', { errors: errors.array(), inputs: {
+          name: req.body.name,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: req.body.password,
+          confirm_password: req.body.confirm_password,
+          }
+        });
       } else {
         bcryptjs.hash(req.body.password, 10 , async (err, hashedPassword) => {
           if (err) throw err;
