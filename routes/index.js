20d0ad7cc635aa4540b09cb0572c7eea8140db83
LucioFurnari <luciofurnari99@gmail.com>
2023-11-21 19:32:04 -0300

    Add log-in get and post routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { index, user_create_post, user_signup_get } = require('../controllers/userControllers');
+const { index, user_create_post, user_signup_get, user_login_get, user_login_post } = require('../controllers/userControllers');
 
 router.get('/', index);
 
 router.get('/sign-up', user_signup_get);
 
 router.post('/sign-up', user_create_post);
+
+router.get('/log-in', user_login_get);
+
+router.post('/log-in', user_login_post);
+
 // Export router
 module.exports = router;
