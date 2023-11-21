668421b76371dac87d0ef5ec10b511c94e224d96
LucioFurnari <luciofurnari99@gmail.com>
2023-11-21 19:51:58 -0300

    Add log out get route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { index, user_create_post, user_signup_get, user_login_get, user_login_post } = require('../controllers/userControllers');
+const { index, user_create_post, user_signup_get, user_login_get, user_login_post, user_logout } = require('../controllers/userControllers');
 
 router.get('/', index);
 
@@ -11,6 +11,8 @@ router.post('/sign-up', user_create_post);
 router.get('/log-in', user_login_get);
 
 router.post('/log-in', user_login_post);
+
+router.get('/log-out', user_logout);
 
 // Export router
 module.exports = router;
