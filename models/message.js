47e0c22a715528f07b1ffea485b9d4bdc06ea529
LucioFurnari <luciofurnari99@gmail.com>
2023-11-27 17:50:18 -0300

    Fix user id in Message Schema

--- models/message.js
+++ models/message.js
@@ -5,7 +5,7 @@ const MessageSchema = Schema({
   title: { type: String, required: true, minLength: 1, maxLength: 100 },
   text: { type: String, required: true, minLength: 1, maxLength: 100 },
   timestamp: { type: Date, default: Date.now, required: true },
-  user: { typ: Schema.type.ObjectId, required: true, ref: 'User' },
+  user: { type: Schema.ObjectId, required: true, ref: 'User' },
 });
 
 // Export model //
