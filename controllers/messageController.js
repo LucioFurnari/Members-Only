af72cc7cc8bead8d9bf150d6476cb1f66abad105
LucioFurnari <luciofurnari99@gmail.com>
2023-12-06 22:41:14 -0300

    Add date fns format to timestamp, and document comments

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,12 +1,15 @@
 const Message = require('../models/message');
 const { body, validationResult } = require('express-validator');
+const { format, parseISO } = require('date-fns');
 
+// Create message controller.
+// The timestamp uses a current date that formats and then saves it as a string.
 exports.create_message = async (req, res) => {
   if (req.isAuthenticated()) {
     const message = new Message({
       title: req.body.title,
       text: req.body.message,
-      timestamp: new Date(),
+      timestamp: format(new Date(), "PPPPp"),
       user: req.user._id
     })
   
