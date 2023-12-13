613cbc8dcb3860e22f320d60cd6ce850e0de3a29
LucioFurnari <luciofurnari99@gmail.com>
2023-12-13 15:40:54 -0300

    Add max length for title and message

--- models/message.js
+++ models/message.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const MessageSchema = Schema({
-  title: { type: String, required: true, minLength: 1, maxLength: 100 },
-  text: { type: String, required: true, minLength: 1 },
+  title: { type: String, required: true, minLength: 1, maxLength: 50 },
+  text: { type: String, required: true, minLength: 1, maxLength: 100 },
   timestamp: { type: String, required: true },
   user: { type: Schema.ObjectId, required: true, ref: 'User' },
 });
