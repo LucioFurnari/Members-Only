69d01a6828ce3ae5bc448aaf6893625da2591148
LucioFurnari <luciofurnari99@gmail.com>
2023-12-12 22:54:08 -0300

    Change port to use env PORT variable

--- app.js
+++ app.js
@@ -41,4 +41,4 @@ app.use((req,res) => {
   res.status(404).render('404');
 })
 
-app.listen(3000, () => console.log('app listening on port 3000!'));
+app.listen(process.env.PORT || 3001, () => console.log('app listening on port 3000!'));
